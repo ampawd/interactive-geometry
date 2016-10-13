c780e362d46064a8236ad6c775bbe10e2907a06d
ampawd <nemanoe@gmail.com>
2016-10-13 17:36:31 +0500

    formatting

--- assets/js/lib/utils.js
+++ assets/js/lib/utils.js
@@ -67,17 +67,17 @@
 		
 	   if (!window.requestAnimationFrame) {
 	      window.requestAnimationFrame = function(callback, element) {
-		   var currTime = new Date().getTime();
-		   var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-		   var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-		   lastTime = currTime + timeToCall;
-		   return id;
+		     var currTime = new Date().getTime();
+		     var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+		     var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+		     lastTime = currTime + timeToCall;
+		     return id;
 		  };
 	   }
 	   if (!window.cancelAnimationFrame) {
-	      window.cancelAnimationFrame = function(id) {
-		   clearTimeout(id);
-	      }
+	       window.cancelAnimationFrame = function(id) {
+	       clearTimeout(id);
+	     }
 	   }
 	}());
 
